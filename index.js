476095322545f20354592f0007a729de8bb419b4
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-05-30 16:05:40 +0600

    i  have added here new updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const jobsCollection = client.db("careerCodeDB").collection("jobs");
     const applicationsCollection = client
       .db("careerCodeDB")
@@ -124,7 +124,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Successfully connected to MongoDB!");
   } finally {
   }
